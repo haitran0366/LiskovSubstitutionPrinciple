d93113eaa380186abed8fec75382a755f2fc5fd4
haitran0366 <haitran_usa@yahoo.com>
2021-08-20 11:35:03 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LSP_Ex_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service service = new Service1();
+            service.DoService();
+        }
+        // The point is that a Service can be replaced by a Service1.
+
+        //Interface Segregation Principle
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP_Ex_
+{
+    class Service1:Service
+    {
+        public override void DoService()
+        {
+            Console.WriteLine("Do service 1");
+        }
+
+    }
+}
